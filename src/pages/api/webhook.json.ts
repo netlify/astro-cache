6b767d96d9fefdc65114b6f650bc612ffc5f2c69
Matt Kane <matt.kane@netlify.com>
2024-02-23 15:33:09 +0000

    Update

--- src/pages/api/webhook.json.ts
+++ src/pages/api/webhook.json.ts
@@ -1,6 +1,17 @@
+import { purgeCache } from '@netlify/functions'
 
-export async function POST({ params }:any) {
+export async function POST({ request }:any) {
+  const body = await request.json()
+  const id = Number(body.entity.id).toString()
 
-console.log({params})
+  purgeCache({ tags: [id] }).catch((error) => {
+    return new Response(JSON.stringify({
+      message: `Failed to revalidate article with id ${id}`,
+      error: error.message,
+    }), { status: 500 });
+  })
 
+  return new Response(JSON.stringify({
+    message: `Revalidated article with id ${id}`,
+  }), { status: 200 });
 }
